a1058d41dc99d10a079d34fe0593b4e5eae36407
hiyuki2578 <tomoka319@gmail.com>
2017-02-14 21:48:47 +0900

    fixed intent

--- app/src/main/java/jp/android_group/student/ticketsplit/MainActivity.java
+++ app/src/main/java/jp/android_group/student/ticketsplit/MainActivity.java
@@ -174,6 +174,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu){
 		getMenuInflater().inflate(R.menu.main, menu);
